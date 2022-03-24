61010d3e518580ecc689779f3785e8e09d01b3ac
jinlow <james.d.inlow@gmail.com>
2022-03-23 20:34:25 -0500

    Pulling State up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -7,85 +6,60 @@ import Form from 'react-bootstrap/Form';
 
 
 class ProfileLoader extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>To get profile data:</p><Button onClick={this.props.dataLoader} variant="primary">{this.props.children}</Button>
+        {
+          this.props.aboutMe && <div>
+            <p>Profile name: {this.props.profileName}</p>
+            <p>Profile info: {this.props.aboutMe}</p>
+            <p>Random number: {this.props.randInt}</p>
+          </div>
+        }
+      </div>
+    )
+  }
+}
+
+export default class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      profile_name: null,
-      about_me: null,
-      rand_int: null,
+      profileName: null,
+      aboutMe: null,
+      randInt: null,
     };
   }
 
   getData() {
     fetch("/profile")
-    .then(response => response.json())
-    .then(res => {
-      this.setState((
-        {
-          profile_name: res.name,
-          about_me: res.about,
-          rand_int: res.number,
-        }
-      ));
-    });
+      .then(response => response.json())
+      .then(res => {
+        this.setState((
+          {
+            profileName: res.name,
+            aboutMe: res.about,
+            randInt: res.number,
+          }
+        ));
+      });
   }
 
   render() {
     return (
-      <div>
-      <p>To get profile data:</p><Button onClick={() => this.getData()} variant="primary">Click Here!</Button>
-        {
-          this.state.profile_name && <div>
-            <p>Profile name: {this.state.profile_name}</p>
-            <p>Profile info: {this.state.about_me}</p>
-            <p>Random number: {this.state.rand_int}</p>
-          </div>
-        }
-        </div>
-    )
+      <div className="App">
+        <header className="App-header">
+          <Form>
+            <Form.Label>Enter Name</Form.Label>
+            <Form.Control type="text" placeholder="Enter a name" />
+            <Form.Text classname="text-muted">
+              Enter the name of a user to select.
+            </Form.Text>
+          </Form>
+          <ProfileLoader profileName={this.state.profileName} aboutMe={this.state.aboutMe} randInt={this.state.randInt} dataLoader={() => this.getData()}>Press Here</ProfileLoader>
+        </header>
+      </div>
+    );
   }
-
 }
-
-function App() {
-  // const [profileData, setProfileData] = useState(null)
-
-  // function getData() {
-  //   fetch("/profile")
-  //   .then(response => response.json())
-  //   .then(res => {
-  //     setProfileData((
-  //       {
-  //         profile_name: res.name,
-  //         about_me: res.about,
-  //         rand_int: res.number,
-  //       }
-  //     ));
-  //   });
-  // }
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Form>
-          <Form.Label>Enter Name</Form.Label>
-          <Form.Control type="text" placeholder="Enter a name" />
-          <Form.Text classname="text-muted">
-            Enter the name of a user to select.
-          </Form.Text>
-        </Form>
-        {/* <p>To get profile data:</p><Button onClick={getData} variant="primary">Click Here</Button>
-        {
-          profileData && <div>
-            <p>Profile name: {profileData.profile_name}</p>
-            <p>Profile info: {profileData.about_me}</p>
-            <p>Random number: {profileData.rand_int}</p>
-          </div>
-        } */}
-       <ProfileLoader/>
-      </header>
-    </div>
-  );
-}
-
-export default App;
